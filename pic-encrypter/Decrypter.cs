1878d0f608c3de1735826d8a92cbb433fb4dd7bb
Danilx8 <lugovskihdanil@yandex.ru>
2023-03-17 17:25:18 +0700

    Switched python code to c# classes

---
+++ pic-encrypter/Decrypter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace pic_encrypter
+{
+    class Decrypter
+    {
+        static void ConvertToText()
+        {
+            Image image = Image.FromFile("text_image.png");
+
+            Bitmap bitmap = new Bitmap(image);
+            Color color;
+
+            string binary_text = "";
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    color = bitmap.GetPixel(x, y);
+                    if (color.R == 255 && color.G == 255 && color.B == 255)
+                    {
+                        binary_text += "0";
+                    }
+                    else
+                    {
+                        binary_text += "1";
+                    }
+                }
+            }
+
+            string text = "";
+            for (int i = 0; i < binary_text.Length; i += 8)
+            {
+                string byteStr = binary_text.Substring(i, 8);
+                char c = (char)Convert.ToInt32(byteStr, 2);
+                text += c.ToString();
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ pic-encrypter/Encrypter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+
+namespace pic_encrypter
+{
+    class Encrypter
+    {
+        static void ConvertToPic(string text)
+        {
+            string binary_text = "";
+            foreach (char c in text)
+            {
+                string binary_char = Convert.ToString(c, 2).PadLeft(8, '0');
+                binary_text += binary_char;
+            }
+
+            int image_width = binary_text.Length / 3 + 1;
+            int image_height = 3;
+
+            Bitmap image = new Bitmap(image_width, image_height);
+
+            for (int y = 0; y < image_height; y++)
+            {
+                for (int x = 0; x < image_width; x++)
+                {
+                    Color color;
+                    if (binary_text.Length > 0)
+                    {
+                        int bit = int.Parse(binary_text.Substring(0, 1));
+                        binary_text = binary_text.Remove(0, 1);
+                        if (bit == 1)
+                        {
+                            color = Color.FromArgb(new Random().Next(256), new Random().Next(256), new Random().Next(256));
+                        }
+                        else
+                        {
+                            color = Color.FromArgb(255, 255, 255);
+                        }
+                    }
+                    else
+                    {
+                        color = Color.FromArgb(255, 255, 255);
+                    }
+                    image.SetPixel(x, y, color);
+                }
+            }
+
+            image.Save("text_image.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
